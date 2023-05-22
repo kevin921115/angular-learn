fb88610bf87bc78a5cc7fed0fc168a587d4be305
kevin921115 <kevin.dev921115@gmail.com>
2023-05-22 23:01:33 +0900

    "Using forms for user input"

---
+++ src/app/cart.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { Product } from './products';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CartService {
+
+  constructor(private http: HttpClient) { }
+
+  items: Product[] = [];
+
+  addToCart(product: Product) {
+    this.items.push(product);
+  }
+
+  getItems() {
+    return this.items;
+  }
+
+  clearCart() {
+    this.items = [];
+    return this.items;
+  }
+  getShippingPrices() {
+    return this.http.get<{type: string, price: number}[]>('/assets/shipping.json');
+  }
+}
---
+++ src/app/shipping/shipping.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ShippingComponent } from './shipping.component';
+
+describe('ShippingComponent', () => {
+  let component: ShippingComponent;
+  let fixture: ComponentFixture<ShippingComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [ShippingComponent]
+    });
+    fixture = TestBed.createComponent(ShippingComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
